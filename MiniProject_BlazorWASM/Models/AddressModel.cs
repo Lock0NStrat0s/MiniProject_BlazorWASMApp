f2f4363b5e8e87f346a7231a5ba45271d2f2a058
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-10-01 14:06:49 -0600

    mini project complete.

---
+++ MiniProject_BlazorWASM/Models/AddressModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniProject_BlazorWASM.Models;
+
+public class AddressModel
+{
+    [Required]
+    public string StreetAddress { get; set; }
+    [Required]
+    public string City { get; set; }
+    [Required]
+    [StringLength(2)]
+    public string Province { get; set; }
+    [Required]
+    [StringLength(6)]
+    public string PostalCode { get; set; }
+}
---
+++ MiniProject_BlazorWASM/Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniProject_BlazorWASM.Models;
+
+public class PersonModel
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+}
